bf4f68db43bae2d413fb522f5f66b626de4bf1c0
SilentAssassin101 <treytonwilkerson@gmail.com>
2024-04-01 03:19:37 -0400

    added fetch to monitor creation

--- src/components/AddMonitorForm.vue
+++ src/components/AddMonitorForm.vue
@@ -5,7 +5,13 @@
         <label for="monitor-ip">Server IP</label>
         <input class="p-1 px-2 rounded-lg" type="text" id="monitor-ip" placeholder="192.168.1.1" required />
         <label for="monitor-port">Server Port</label>
-        <input class="p-1 px-2 rounded-lg" type="number" id="monitor-port" placeholder="80" />
+        <input class="p-1 px-2 rounded-lg" type="number" id="monitor-port" placeholder="8080" required />
+        <label for="monitor-webhook">Webhook URL</label>
+        <input class="p-1 px-2 rounded-lg" type="url" id="monitor-webhook"
+            placeholder="https://discord.com/api/webhooks/1234567890/abcdefg" required />
+        <label for="monitor-authcode">Auth Code</label>
+        <input class="p-1 px-2 rounded-lg" type="password" id="monitor-authcode" placeholder="Authorization Code"
+            required />
         <DefaultButton text="Add Monitor" />
     </form>
 </template>
@@ -16,6 +22,36 @@ import DefaultButton from './DefaultButton.vue';
 export default {
     components: {
         DefaultButton
+    },
+    methods: {
+        async submitForm() {
+            const formData = {
+                name: document.getElementById('monitor-name').value,
+                address: document.getElementById('monitor-ip').value,
+                port: document.getElementById('monitor-port').value,
+                webhook: document.getElementById('monitor-webhook').value,
+                authcode: document.getElementById('monitor-authcode').value
+            };
+
+            try {
+                const response = await fetch('teach.civilhost.net/createmonitor.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('There was a problem with the fetch operation: ', error);
+            }
+        }
     }
 }
 </script>
